2cec642efa923cd49dc556b2795fae25715058ff
wuti1609 <yma@developserver.10-3-12-11.xdcb.ucloud-bj-bgp-c.dm>
2015-09-28 10:38:46 +0800

    encap PredictArticle object

--- src/main/scala/com/dataman/omega/service/server/ClusterService.scala
+++ src/main/scala/com/dataman/omega/service/server/ClusterService.scala
@@ -34,43 +34,12 @@ trait ClusterService extends WebService {
     RawHeader("Access-Control-Allow-Headers", "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, Cache-Control, X-XSRFToken, Authorization") :: Nil
 
   val clusterServiceRoutes = {
-    pathPrefix("a") {
-      post {
-        entity(as[Args]) {
-          args => {
-            complete(args.name + "\t" + args.age)
-          }
-        }
-      }
-    } ~ pathPrefix("b") {
+    pathPrefix("predArt") {
       post {
         formField('msg.as[String]) {
           msg => {
-            complete(msg + "yma")
-          }
-        }
-      }
-    } ~ pathPrefix("predArt") {
-      post {
-        formField('msg.as[String]) {
-          msg => {
-            val jsonMsg = Base64Util.decodeBase64withUTF8(msg)
-            implicit val inputJsonFormat = jsonFormat6(InputMsg)
-            implicit val outputJsonFormat = jsonFormat6(OutputMsg)
-            val msgbean = jsonMsg.parseJson.convertTo[InputMsg]
-            Mysql.insertMessage(msgbean)
-            val (articles, keywords) = predictArticle.getArticle(Analyzer.sc,
-                                               Analyzer.sqlContext,
-                                               Analyzer.model,
-                                               msgbean.articleid,
-                                               msgbean.appid,
-                                               msgbean.content.get,
-                                               Analyzer.analyzer)
-
-
-            val oum = OutputMsg(msgbean.articleid, msgbean.appid, Some(keywords), Some(articles), Option(null), Option(null))
-println(oum.toJson.toString())
-            complete(oum.toJson.toString())
+            val oum = PredictArticle.service(msg)
+            complete(oum)
           }
         }
       }
---
+++ src/main/scala/com/dataman/omega/service/server/PredictArticle.scala
@@ -0,0 +1,35 @@
+package com.dataman.omega.service.server
+
+import com.dataman.nlp.predictArticle
+import com.dataman.omega.service.data.{Mysql, OutputMsg, InputMsg}
+import com.dataman.webservice.{Analyzer, Base64Util}
+import spray.json.DefaultJsonProtocol
+import spray.json._
+import DefaultJsonProtocol._
+import spray.routing.Route._
+import spray.httpx.SprayJsonSupport._
+import scala.collection.JavaConversions._
+
+/**
+ * Created by mymac on 15/9/27.
+ */
+object PredictArticle {
+  def service(msg: String) = {
+      val jsonMsg = Base64Util.decodeBase64withUTF8(msg)
+      implicit val inputJsonFormat = jsonFormat6(InputMsg)
+      implicit val outputJsonFormat = jsonFormat6(OutputMsg)
+      val msgbean = jsonMsg.parseJson.convertTo[InputMsg]
+      Mysql.insertMessage(msgbean)
+      val (articles, keywords) = predictArticle.getArticle(Analyzer.sc,
+        Analyzer.sqlContext,
+        Analyzer.model,
+        msgbean.articleid,
+        msgbean.appid,
+        msgbean.content.get,
+        Analyzer.analyzer)
+      val oum = OutputMsg(msgbean.articleid, msgbean.appid, Some(keywords), Some(articles), Option(null), Option(null))
+
+      Base64Util.encodeUTF8String(oum.toJson.toString())
+  }
+}
+
